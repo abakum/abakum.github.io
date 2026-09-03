b79a76c602970148e221cab30a37b4415ae2437c
abakum <koka.abakum@gmail.com>
2026-09-03 14:32:10 +0300

    chore(vk): refresh screenshots, add dark-theme shots

--- LunarReturns/vk/screenshots.bat
+++ LunarReturns/vk/screenshots.bat
@@ -1,2 +1,4 @@
-chrome.exe --headless --window-size=600,1200 --screenshot="C:\tmp\screenshot.png" "https://abakum.github.io/LunarReturns/"
-chrome.exe --headless --window-size=600,1200 --screenshot="C:\tmp\screenshot2.png" "https://abakum.github.io/LunarReturns/#1"
+"C:\Program Files\Google\Chrome\Application\chrome.exe" --headless --window-size=600,1200 --screenshot="c:\tmp\screenshot.png" "https://abakum.github.io/LunarReturns/?vk_app_id=1"
+"C:\Program Files\Google\Chrome\Application\chrome.exe" --headless --window-size=600,1200 --screenshot="c:\tmp\screenshot2.png" "https://abakum.github.io/LunarReturns/?vk_app_id=1#1"
+"C:\Program Files\Google\Chrome\Application\chrome.exe" --headless --window-size=600,1200 --screenshot="c:\tmp\screenshot3.png" "https://abakum.github.io/LunarReturns/?vk_app_id=1&appearance=dark"
+"C:\Program Files\Google\Chrome\Application\chrome.exe" --headless --window-size=600,1200 --screenshot="c:\tmp\screenshot4.png" "https://abakum.github.io/LunarReturns/?vk_app_id=1&appearance=dark#1"
